9c00f2e625c58feff10fd2f76e7a8614440627a0
Eivind Melleby <emelleby@gmail.com>
2022-04-27 01:00:37 +0200

    Fix App js

--- api/src/lib/auth.js
+++ api/src/lib/auth.js
@@ -1,28 +1,38 @@
+import { parseJWT } from '@redwoodjs/api'
 import { AuthenticationError, ForbiddenError } from '@redwoodjs/graphql-server'
-import { db } from './db'
+import { logger } from 'src/lib/logger'
 
 /**
- * The session object sent in as the first argument to getCurrentUser() will
- * have a single key `id` containing the unique ID of the logged in user
- * (whatever field you set as `authFields.id` in your auth function config).
- * You'll need to update the call to `db` below if you use a different model
- * name or unique field name, for example:
+ * getCurrentUser returns the user information together with
+ * an optional collection of roles used by requireAuth() to check
+ * if the user is authenticated or has role-based access
  *
- *   return await db.profile.findUnique({ where: { email: session.id } })
- *                   ───┬───                       ──┬──
- *      model accessor ─┘      unique id field name ─┘
+ * @param decoded - The decoded access token containing user info and JWT claims like `sub`. Note could be null.
+ * @param { token, SupportedAuthTypes type } - The access token itself as well as the auth provider type
+ * @param { APIGatewayEvent event, Context context } - An object which contains information from the invoker
+ * such as headers and cookies, and the context information about the invocation such as IP Address
  *
- * !! BEWARE !! Anything returned from this function will be available to the
- * client--it becomes the content of `currentUser` on the web side (as well as
- * `context.currentUser` on the api side). You should carefully add additional
- * fields to the `select` object below once you've decided they are safe to be
- * seen if someone were to open the Web Inspector in their browser.
+ * @see https://github.com/redwoodjs/redwood/tree/main/packages/auth for examples
  */
-export const getCurrentUser = async (session) => {
-  return await db.user.findUnique({
-    where: { id: session.id },
-    select: { id: true, email: true },
-  })
+export const getCurrentUser = async (
+  decoded,
+  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+  { token, type },
+  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+  { event, context }
+) => {
+  if (!decoded) {
+    logger.warn('Missing decoded user')
+    return null
+  }
+
+  const { roles } = parseJWT({ decoded })
+
+  if (roles) {
+    return { ...decoded, roles }
+  }
+
+  return { ...decoded }
 }
 
 /**
@@ -33,6 +43,16 @@ export const getCurrentUser = async (session) => {
 export const isAuthenticated = () => {
   return !!context.currentUser
 }
+
+/**
+ * When checking role membership, roles can be a single value, a list, or none.
+ * You can use Prisma enums too (if you're using them for roles), just import your enum type from `@prisma/client`
+ */
+
+/**
+ * When checking role membership, roles can be a single value, a list, or none.
+ * You can use Prisma enums too (if you're using them for roles), just import your enum type from `@prisma/client`
+ */
 
 /**
  * Checks if the currentUser is authenticated (and assigned one of the given roles)
